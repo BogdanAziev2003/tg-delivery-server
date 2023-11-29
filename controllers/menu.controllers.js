0876e936fd7be8d191db97cc555b76d30daa93e4
BogdanAziev2003 <neazievbogdan@gmail.com>
2023-11-29 17:13:43 +0300

    get orders

--- controllers/menu.controllers.js
+++ controllers/menu.controllers.js
@@ -1,118 +1,121 @@
-import db from '../db.js'
-import { fileURLToPath } from 'url'
-import path, { dirname } from 'path'
+import db from "../db.js";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
 
 export const getAllMenu = async (req, res) => {
   try {
-    const menu = await (await db.query("select * from goods where instock = 'true'")).rows
-    const modifiers = await (await db.query('select * from modifiers')).rows
+    const menu = await (
+      await db.query("select * from goods where instock = 'true'")
+    ).rows;
+    const modifiers = await (await db.query("select * from modifiers")).rows;
     const goods_modifiers = await (
-      await db.query('select * from goods_modifiers')
-    ).rows
+      await db.query("select * from goods_modifiers")
+    ).rows;
     menu.forEach((product) => {
-      const id = product.id
+      const id = product.id;
       let mod = goods_modifiers
         .filter((el) => el.goods_id === id)
-        .map((el) => el.modifiers_id)
-      product.modifiers = modifiers.filter((el) => mod.includes(el.id))
+        .map((el) => el.modifiers_id);
+      product.modifiers = modifiers.filter((el) => mod.includes(el.id));
       product.modifiers.forEach((el) => {
-        el.amount = 0
-      })
-      if (product.category === 'Сэндвичи') {
-        product.snack = 'Фри'
-        product.sause = 'Кетчуп'
+        el.amount = 0;
+      });
+      if (product.category === "Сэндвичи") {
+        product.snack = "Фри";
+        product.sause = "Кетчуп";
       }
-    })
+    });
 
-    let sortedMenu = menu.sort((a, b) => a.id - b.id)
+    let sortedMenu = menu.sort((a, b) => a.id - b.id);
 
-    res.status(200).json(sortedMenu)
+    res.status(200).json(sortedMenu);
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'Не удалось получить меню' })
+    console.log(error);
+    res.status(500).json({ message: "Не удалось получить меню" });
   }
-}
+};
 
 export const changeStock = async (req, res) => {
   try {
-    const { id, inStock } = req.body
+    const { id, inStock } = req.body;
     const data = await (
       await db.query(`update goods set inStock = ${inStock} where id = ${id}`)
-    ).rows
+    ).rows;
 
-    res.status(201).json(data)
+    res.status(201).json(data);
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'Не удалось изсенить поле' })
+    console.log(error);
+    res.status(500).json({ message: "Не удалось изсенить поле" });
   }
-}
+};
 
 export const createOrder = async (req, res) => {
   try {
-    const { username, tgId, order, price } = req.body
+    const { username, tgId, order, price } = req.body;
     let createdTime = new Date(
       Date.now() + 1000 * 60 * -new Date().getTimezoneOffset()
     )
       .toISOString()
-      .replace('T', ' ')
-      .replace('Z', '')
-    console.log(createdTime)
+      .replace("T", " ")
+      .replace("Z", "");
+    console.log(createdTime);
     let resText = db.query(
       `insert into orders (username, tg_id, order_positions, created_on, price) values ($1, $2, $3, $4, $5)`,
       [username, tgId, order, createdTime, price]
-    )
-    res.status(200).json(resText)
+    );
+    res.status(200).json(resText);
   } catch (error) {
-    console.log(error)
-    res.status(500).json('Не удалось создать заказ')
+    console.log(error);
+    res.status(500).json("Не удалось создать заказ");
   }
-}
+};
 
 export const getOriginOrders = async (req, res) => {
   try {
-    res.status(200).download('./file.xlsx')
+    const orders = await (await db.query("select * from origin_orders")).rows;
+    res.json(orders);
   } catch (error) {
-    console.log(error)
-    res.status(500).json('Не удалось создать заказ')
+    console.log(error);
+    res.status(500).json("Не удалось создать заказ");
   }
-}
-
+};
 
 export const createOriginOrder = async (req, res) => {
   try {
-    const { username, tgId, order, price, isAccepted } = req.body
+    const { username, tgId, order, price, isAccepted } = req.body;
     let createdTime = new Date(
       Date.now() + 1000 * 60 * -new Date().getTimezoneOffset()
     )
       .toISOString()
-      .replace('T', ' ')
-      .replace('Z', '')
-    console.log(createdTime)
+      .replace("T", " ")
+      .replace("Z", "");
+    console.log(createdTime);
     let resText = db.query(
       `insert into origin_orders (username, tg_id, order_positions, created_on, price, is_acceped) values ($1, $2, $3, $4, $5, $6)`,
       [username, tgId, order, createdTime, price, isAccepted]
-    )
-    res.status(200).json(resText)
+    );
+    res.status(200).json(resText);
   } catch (error) {
-    console.log(error)
-    res.status(500).json('Не удалось создать заказ')
+    console.log(error);
+    res.status(500).json("Не удалось создать заказ");
   }
-}
+};
 
 export const getOrders = async (req, res) => {
   try {
-    res.status(200).download('./file.xlsx')
+    const orders = await (await db.query("select * from orders")).rows;
+    res.json(orders);
   } catch (error) {
-    console.log(error)
-    res.status(500).json('Не удалось создать заказ')
+    console.log(error);
+    res.status(500).json("Не удалось создать заказ");
   }
-}
+};
 
 export const getImage = (req, res) => {
-  let imageName = req.params.imageName
+  let imageName = req.params.imageName;
 
-  const __filename = fileURLToPath(import.meta.url)
-  const __dirname = dirname(__filename)
-  console.log(__dirname)
-  res.sendFile(path.join(__dirname, './../images/Compressed', imageName))
-}
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname, "./../images/Compressed", imageName));
+};
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
-import menuRouter from './router/menu.router.js'
+import menuRouter from "./router/menu.router.js";
 
-dotenv.config()
-const PORT = process.env.PORT || 8080
+dotenv.config();
+const PORT = process.env.PORT || 8080;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors({
-  origin: '*', // Разрешить запросы от любого источника
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-}))
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*", // Разрешить запросы от любого источника
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
 
-app.use('/api/menu', menuRouter)
+app.use("/api/menu", menuRouter);
